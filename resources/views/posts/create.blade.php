dae13fc5fdb2649c5e10ff0a1d53319a404e1207
Wildan Belfiore <mikewildan@outlook.com>
2021-05-04 16:55:30 +0700

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,60 @@
+@extends('template')
+ 
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Tambah Data</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('posts.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+ 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+ 
+<form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+ 
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="Nama">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <textarea class="form-control" style="height:150px" name="alamat" placeholder="Alamat"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Tanggal Lahir:</strong>
+                <textarea class="form-control" style="height:150px" name="tgllhr" placeholder="Tanggal Lahir"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Hobby:</strong>
+                <textarea class="form-control" style="height:150px" name="hobbi" placeholder="Hobby"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+ 
+</form>
+@endsection
